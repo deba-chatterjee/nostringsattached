77dde215a0281153980420d6df05a32270e998db
deba-chatterjee <42351486+deba-chatterjee@users.noreply.github.com>
2026-01-25 12:43:13 -0800

    Update vite.config.ts
    Changes to support custom domain in github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
-  base: process.env.GITHUB_REPOSITORY
-    ? `/${process.env.GITHUB_REPOSITORY.split("/")[1]}/`
-    : "/",
+  base: "/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
